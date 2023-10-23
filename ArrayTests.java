6d275853072c2d49f47e5aa5b21ad71af43791e2
MinteaAliens <lauren.mintea@gmail.com>
2023-10-23 16:39:01 -0700

    Fixing bugs to ArrayExamples.class

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  // One symptom of a bug in reverseInPlace() is that
+  // you won't actually get a reversed array in an asymmetric array
+  @Test
+  public void testReverseInPlace1(){
+    int [] input2 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] {3, 2, 1}, input2);
+  }
+
+  // One symptom of a bug in reverse is that we get a blank array
+  @Test
+  public void testReversed1(){
+    int[] input2 = {1, 2, 3};
+    assertArrayEquals(new int[] {3, 2, 1}, ArrayExamples.reversed(input2));
+  }
 }
